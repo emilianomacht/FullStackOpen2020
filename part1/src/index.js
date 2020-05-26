33c96fe694c1cf5f8dd64930e9c9629ea2f2b50a
Emiliano Macht <emilianomacht@protonmail.com>
2020-05-26 11:03:26 -0300

    ex 1.7

--- part1/src/index.js
+++ part1/src/index.js
@@ -14,6 +14,9 @@ const Statistics = ({ feedback }) => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad) * 100} %</p>
     </div>
   );
 };
