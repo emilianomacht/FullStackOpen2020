229a7ee969f573f36e1803c779b96a327dfd29a7
Emiliano Macht <emilianomacht@protonmail.com>
2020-05-27 11:14:47 -0300

    create-react-app for part2

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,57 +1,43 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const Button = ({ text, handleClick }) => (
-  <button onClick={handleClick}>{text}</button>
-);
-
-const ShowAnecdote = ({ singleAnecdote, numberOfVotes }) => (
-  <div>
-    <p>{singleAnecdote}</p>
-    <p>Has {numberOfVotes} votes.</p>
-  </div>
-);
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
 
 const App = (props) => {
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
-
-  const randomSelect = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length));
-  };
-
-  const voteAnecdote = () => {
-    const votesCopy = [...votes];
-    votesCopy[selected] += 1;
-    setVotes(votesCopy);
-  };
+  const { notes } = props
 
   return (
     <div>
-      <h3>Anecdote of the day</h3>
-      <ShowAnecdote
-        singleAnecdote={props.anecdotes[selected]}
-        numberOfVotes={votes[selected]}
-      />
-      <Button text="vote" handleClick={voteAnecdote} />
-      <Button text="next anecdote" handleClick={randomSelect} />
-
-      <h3>Anecdote with most votes</h3>
-      <ShowAnecdote
-        singleAnecdote={props.anecdotes[votes.indexOf(Math.max(...votes))]}
-        numberOfVotes={votes[votes.indexOf(Math.max(...votes))]}
-      />
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
     </div>
-  );
-};
+  )
+}
 
-const anecdotes = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
