12e9e90052b4841c6101549f50747ef406c2d568
Emiliano Macht <emilianomacht@protonmail.com>
2020-05-28 10:33:29 -0300

    phonebook folder

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
+const App = () => {
+  // const [persons, setPersons] = useState([
+  //   { name: "Arto Hellas", number: "040-123456" },
+  //   { name: "Ada Lovelace", number: "39-44-5323523" },
+  //   { name: "Dan Abramov", number: "12-43-234345" },
+  //   { name: "Mary Poppendieck", number: "39-23-6423122" },
+  // ]);
+
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+    });
+  }, []);
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter handleFilter={handleFilter} filter={filter} />
+
+      <h3>Add new person</h3>
+
+      <PersonForm
+        addPerson={addPerson}
+        handleNewName={handleNewName}
+        newName={newName}
+        handleNewNumber={handleNewNumber}
+        newNumber={newNumber}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={persons} filter={filter} />
+    </div>
+  );
+};
+
+export default App;
