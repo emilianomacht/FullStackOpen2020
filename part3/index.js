1b2bbfd5d697f73666b385654181700e542684dc
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-02 12:02:31 -0300

    ex 3.13 complete

--- part3/index.js
+++ part3/index.js
@@ -1,47 +1,22 @@
 // const http = require('http')
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
-const Person = require('./models/person');
+const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 
 app.use(cors());
-app.use(express.static('build'));
+app.use(express.static("build"));
 app.use(express.json());
 
 morgan.token("JSON-POST", (req, res) => JSON.stringify(req.body));
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :JSON-POST"));
-
-// let persons = [
-//   {
-//     name: "Arto Hellas",
-//     number: "22222222222",
-//     id: 1,
-//   },
-//   {
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//     id: 2,
-//   },
-//   {
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//     id: 3,
-//   },
-//   {
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//     id: 4,
-//   },
-//   {
-//     name: "EMI",
-//     number: "1234890",
-//     id: 5
-//   }
-// ];
-
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :JSON-POST"
+  )
+);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
@@ -56,17 +31,25 @@ app.get("/api/persons", (req, res) => {
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const personToSend = persons.find((person) => person.id === id);
-
-  personToSend ? res.send(personToSend) : res.status(404).end();
+  const id = req.params.id;
+  Person.findById(id).then((result) => {
+    result !== null 
+    ? res.json(result) 
+    : res.status(404).end();
+  }).catch(error => {
+    console.log(error);
+    res.status(404).end();
+  });
 });
 
 app.get("/info", (req, res) => {
   const curDate = new Date();
-  let stringToSend = `<p>Phonebook has info for ${persons.length} people</p>`;
-  stringToSend += `<p>${curDate}</p>`;
-  res.send(stringToSend);
+  let stringToSend = '';
+  Person.count({}).then(count => {
+    stringToSend += `<p>Phonebook has info for ${count} people</p>`;
+    stringToSend += `<p>${curDate}</p>`;
+    res.send(stringToSend);
+  })
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -106,7 +89,7 @@ app.post("/api/persons", (req, res) => {
 
 // const port = 3001;
 // app.listen(port);
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
