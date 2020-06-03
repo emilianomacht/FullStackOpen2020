566c153e911ecfe48f46d7896362ee58dcbb6f79
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-03 09:28:09 -0300

    ex 4.5

--- part4/tests/listHelper.test.js
+++ part4/tests/listHelper.test.js
@@ -7,7 +7,7 @@ const listHelper = require('../utils/list_helper')
 //   expect(result).toBe(1)
 // })
 
-describe('total likes', () => {
+describe('blogs tests', () => {
   const blogs = [{ _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0 },
     { _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0 },
     { _id: '5a422b3a1b54a676234d17f9', title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12, __v: 0 },
@@ -19,4 +19,15 @@ describe('total likes', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
   })
+
+  test('favorite: blog with most likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    })
+  })
+
+
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,7 +7,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, post) => post.likes + sum, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const sortedBlogs = [...blogs]
+  sortedBlogs.sort((postA, postB) => postB.likes - postA.likes)
+  return ({
+    title: sortedBlogs[0].title,
+    author: sortedBlogs[0].author,
+    likes: sortedBlogs[0].likes
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
