09c56c677d3c1379207e1b29643e1699bbacb936
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-03 09:16:40 -0300

    ex 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, post) => post.likes + sum, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
