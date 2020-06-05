b7b1f0f533d8ca991bf5869c1903def809e08625
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-05 19:45:01 -0300

    ex 4.20

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -28,7 +28,17 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  // console.log('request.token from tokenExtractor', request.token)
+  next()
+}
+
 module.exports = {
+  tokenExtractor,
   requestLogger,
   unknownEndpoint,
   errorHandler
