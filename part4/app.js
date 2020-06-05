bf21018135842c9935ba8ac7599661fc2b8337e0
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-05 19:05:10 -0300

    ex 4.18

--- part4/app.js
+++ part4/app.js
@@ -9,6 +9,7 @@ const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 // Connection to MongoDB
 mongoose.set('useFindAndModify', false)
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
