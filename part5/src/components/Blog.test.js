df3c3a06254deb31448bb931486cb4a23247270b
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-08 16:07:11 -0300

    ex 5.15

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -45,6 +45,22 @@ test('url, likes and username are shown when view button is clicked', () => {
   expect(component.container.querySelector('.username')).toHaveTextContent('test-user-name')
 })
 
+test('handleLike is called twice if button is clicked twice', () => {
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <Blog blog={blog} user={user} handleNewLike={mockHandler} />
+  )
+  const viewButton = component.getByText('view')
+  fireEvent.click(viewButton)
+
+  const likeButton = component.getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
+
 // expect(component.container).toHaveClass('title')
 // expect(component.container.querySelector('.title')).toHaveTextContent('test-title')
 // expect(component.container).toHaveClass('author')
