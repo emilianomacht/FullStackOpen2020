3ef96768ca67c527dbb1a5dddd578478f6d9d19e
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-08 16:02:21 -0300

    ex 5.14

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,38 +1,59 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
+const blog = {
+  title: 'test-title',
+  author: 'test-author',
+  url: 'test-url',
+  likes: 12390,
+  user: {
+    name: 'test-user-name'
+  }
+}
+
+const user = {
+  username: 'test-user-name'
+}
+
 test('component blog renders only title and author', () => {
-  const blog = {
-    title: 'test-title',
-    author: 'test-author',
-    url: 'test-url',
-    likes: 12390,
-    user: {
-      name: 'test-user-name'
-    }
-  }
-
-  const user = {
-    username: 'test-user-name'
-  }
-
   const component = render(
     <Blog blog={blog} user={user} />
   )
 
-  // expect(component.container).toHaveClass('title')
-  // expect(component.container.querySelector('.title')).toHaveTextContent('test-title')
-  // expect(component.container).toHaveClass('author')
-  // expect(component.container.querySelector('.author')).toHaveTextContent('test-author')
   expect(component.container.querySelector('.blog')).toHaveTextContent('test-title')
   expect(component.container.querySelector('.blog')).toHaveTextContent('test-author')
   expect(component.container).not.toHaveClass('.url')
-  // expect(component.container.querySelector('.url')).not.toHaveTextContent('test-url')
   expect(component.container).not.toHaveClass('.likes')
-  // expect(component.container.querySelector('.likes')).not.toHaveTextContent('12390')
   expect(component.container).not.toHaveClass('.username')
-  // expect(component.container.querySelector('.username')).not.toHaveTextContent('test-url')
 
 })
+
+test('url, likes and username are shown when view button is clicked', () => {
+  const component = render(
+    <Blog blog={blog} user={user} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container.querySelector('.blog')).toHaveTextContent('test-title')
+  expect(component.container.querySelector('.blog')).toHaveTextContent('test-author')
+  expect(component.container.querySelector('.url')).toHaveTextContent('test-url')
+  expect(component.container.querySelector('.likes')).toHaveTextContent('12390')
+  expect(component.container.querySelector('.username')).toHaveTextContent('test-user-name')
+})
+
+// expect(component.container).toHaveClass('title')
+// expect(component.container.querySelector('.title')).toHaveTextContent('test-title')
+// expect(component.container).toHaveClass('author')
+// expect(component.container.querySelector('.author')).toHaveTextContent('test-author')
+// expect(component.container.querySelector('.blog')).toHaveTextContent('test-title')
+// expect(component.container.querySelector('.blog')).toHaveTextContent('test-author')
+// expect(component.container).not.toHaveClass('.url')
+// expect(component.container.querySelector('.url')).not.toHaveTextContent('test-url')
+// expect(component.container).not.toHaveClass('.likes')
+// expect(component.container.querySelector('.likes')).not.toHaveTextContent('12390')
+// expect(component.container).not.toHaveClass('.username')
+// expect(component.container.querySelector('.username')).not.toHaveTextContent('test-url')
