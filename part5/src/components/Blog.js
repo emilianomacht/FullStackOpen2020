7f27198b0552557775ef1bb5503602ca83264bad
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-08 11:32:35 -0300

    ex 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+
+import React, { useState } from 'react'
+import '../styles/Blog.css'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const details = (
+    <div>
+      <p>blog.url</p>
+      <p>likes {blog.likes}{' '}
+        <button>like</button>
+      </p>
+      <p>{blog.user.name}</p>
+    </div>
+  )
+
+  return (
+    <div className='blog'>
+      {blog.title} {blog.author} {' '}
+      <button onClick={toggleShowDetails}>{showDetails ? 'hide' : 'view'}</button>
+      {showDetails ? details : null}
+    </div>
+  )
+}
 
 export default Blog
