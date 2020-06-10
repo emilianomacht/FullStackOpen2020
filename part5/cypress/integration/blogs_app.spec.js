be9ef5b72c3c878bed57e7e4e28f04457388859e
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-10 14:56:47 -0300

    ex 5.19

--- part5/cypress/integration/blogs_app.spec.js
+++ part5/cypress/integration/blogs_app.spec.js
@@ -28,13 +28,44 @@ describe('Blog app', function() {
       cy.contains('test-name logged in')
     })
 
-    it.only('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function() {
       cy.contains('login').click()
       cy.get('#username').type('wrong-username')
       cy.get('#password').type('wrong-password')
       cy.get('#login-button').click()
       cy.contains('Wrong username or password')
       cy.get('.negative').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+  })
+
+  describe.only('When logged in', function() {
+    beforeEach(function() {
+      cy.request('POST', 'http://localhost:3001/api/login', {
+        username: 'test-user', password: 'test-password'
+      }).then(response => {
+        localStorage.setItem('loggedUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:3000')
+      })
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('create new blog').click()
+      const title = 'test-title'
+      const author = 'test-author'
+      const url = 'test-url'
+      cy.get('#title').type(title)
+      cy.get('#author').type(author)
+      cy.get('#url').type(url)
+      // cy.contains('create').click()
+      cy.get('button[type=submit]').should('contain', 'create').click()
+
+      cy.contains(`New blog post by ${author} added`)
+      cy.get('.positive').should('have.css', 'color', 'rgb(0, 128, 0)')
+
+      cy.contains(`${title} ${author}`)
+      cy.contains('view').click()
+      cy.contains(`${url}`)
+      cy.contains('likes 0')
     })
   })
 })
