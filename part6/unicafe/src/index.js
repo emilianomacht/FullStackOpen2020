9505be6232fab2ffcee80c227040a820474425c7
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-15 14:33:11 -0300

    moved src to unicafe folder & prepared anecdote src

---
+++ part6/unicafe/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import counterReducer from './reducers/counterReducer';
+
+const store = createStore(counterReducer);
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD',
+    });
+  };
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK',
+    });
+  };
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD',
+    });
+  };
+
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO',
+    });
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={good}>good</button>
+      <button type="button" onClick={ok}>neutral</button>
+      <button type="button" onClick={bad}>bad</button>
+      <button type="button" onClick={zero}>reset stats</button>
+      <div>
+        good
+        {' '}
+        {store.getState().good}
+      </div>
+      <div>
+        neutral
+        {' '}
+        {store.getState().ok}
+      </div>
+      <div>
+        bad
+        {' '}
+        {store.getState().bad}
+      </div>
+    </div>
+  );
+};
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
+store.subscribe(renderApp);
