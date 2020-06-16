9aebdf07af04172811d4bfa9ebe49d6587a56270
Emiliano Macht <emilianomacht@protonmail.com>
2020-06-16 16:23:19 -0300

    ex 6.19

--- part6/src/components/AnecdoteList.js
+++ part6/src/components/AnecdoteList.js
@@ -1,21 +1,25 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { voteAnecdote } from '../reducers/anecdoteReducer';
-import { setNotification } from '../reducers/notificationReducer';
+// import { useDispatch, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
+// import { voteAnecdote } from '../reducers/anecdoteReducer';
+// import { setNotification } from '../reducers/notificationReducer';
 
-const AnecdoteList = () => {
-  const dispatch = useDispatch();
-  const anecdotes = useSelector((state) => state.anecdotes
-    .filter((anecdote) => anecdote.content.toLowerCase().match(state.filter.toLowerCase())));
+// eslint-disable-next-line arrow-body-style
+const AnecdoteList = ({ anecdotes, filter }) => {
+// const dispatch = useDispatch();
+// const anecdotes = useSelector((state) => state.anecdotes
+//   .filter((anecdote) => anecdote.content.toLowerCase().match(state.filter.toLowerCase())));
 
   const vote = (anecdote) => {
-    dispatch(voteAnecdote(anecdote.id));
-    dispatch(setNotification(`You voted '${anecdote.content}'`, 5));
+  // dispatch(voteAnecdote(anecdote.id));
+  // dispatch(setNotification(`You voted '${anecdote.content}'`, 5));
   };
 
   return (
     <div>
       {anecdotes
+        .filter((anecdote) => anecdote.content.toLowerCase().match(filter.toLowerCase()))
         .sort((A, B) => B.votes - A.votes)
         .map((anecdote) => (
           <div key={anecdote.id}>
@@ -34,4 +38,18 @@ const AnecdoteList = () => {
   );
 };
 
-export default AnecdoteList;
+const mapStateToProps = (state) => {
+  // sometimes it is useful to console log from mapStateToProps
+  // console.log(state);
+  return ({
+    anecdotes: state.anecdotes,
+    filter: state.filter,
+  });
+};
+
+// export default AnecdoteList;
+const ConnectedList = connect(
+  mapStateToProps,
+)(AnecdoteList);
+
+export default ConnectedList;
