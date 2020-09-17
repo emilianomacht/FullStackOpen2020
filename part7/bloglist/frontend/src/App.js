7f35efc32169d4dedfa16f5fc2d8f46b96d3587f
Emiliano Macht <emilianomacht@protonmail.com>
2020-09-17 08:06:49 -0300

    ex 7.16

--- part7/bloglist/frontend/src/App.js
+++ part7/bloglist/frontend/src/App.js
@@ -1,36 +1,50 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, { useEffect } from 'react';
-import './App.css';
+import './styles/App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   BrowserRouter as Router,
-  Switch, Route,
+  Switch, Route, Link,
 } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
-import HomePage from './components/HomePage';
+// import HomePage from './components/HomePage';
 import UsersView from './components/UsersView';
 import NewBlog from './components/NewBlog';
 import BlogList from './components/BlogList';
 import User from './components/User';
-
-import { initializeUser } from './reducers/userReducer';
+import { initializeUser, logoutUser } from './reducers/userReducer';
 import BlogPage from './components/BlogPage';
+import { initializeBlogs } from './reducers/blogsReducer';
 
 const App = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
 
   useEffect(() => {
+    dispatch(initializeBlogs());
     dispatch(initializeUser());
   }, [dispatch]);
 
+  if (!user) return null;
+
   return (
     <Router>
+      <div className="nav">
+        <Link to="/blogs">blogs</Link>
+        <Link to="/users">users</Link>
+        <p>
+          {user.name}
+          {' '}
+          logged in
+        </p>
+        <button type="button" onClick={() => dispatch(logoutUser())}>log out</button>
+      </div>
+
       <Switch>
         <Route path="/users/:id">
           {user ? (
             <>
-              <HomePage />
+              {/* <HomePage /> */}
               <User />
             </>
           ) : <LoginPage />}
@@ -38,7 +52,7 @@ const App = () => {
         <Route path="/users">
           {user ? (
             <>
-              <HomePage />
+              {/* <HomePage /> */}
               <UsersView />
             </>
           ) : <LoginPage />}
@@ -47,7 +61,7 @@ const App = () => {
         <Route path="/blogs/:id">
           {user ? (
             <>
-              <HomePage />
+              {/* <HomePage /> */}
               <BlogPage />
             </>
           ) : <LoginPage />}
@@ -55,7 +69,7 @@ const App = () => {
         <Route path="/">
           {user ? (
             <>
-              <HomePage />
+              {/* <HomePage /> */}
               <NewBlog />
               <BlogList />
             </>
